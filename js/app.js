3315a29ff6a36e832bae32d8d75d92b70c5c3d3e
Iván Giovanazzi <ivanog@pm.me>
2020-05-22 23:30:32 -0300

    added functions skeleton

--- js/app.js
+++ js/app.js
@@ -1,2 +1,44 @@
-'use strict'
+'use strict' 
 
+var warnStack = {
+   name: [],
+   surname: [],
+   email: [],
+   age: []
+}
+
+const checkName = function () {
+   var name = document.getElementById('name')
+   console.log(name.value)
+}
+
+const checkSurname = function (surname) {
+   //TODO
+}
+
+const checkEmail = function (email) {
+   //TODO
+}
+
+const checkAge = function (age) {
+   //TODO
+}
+
+const validator = function () {
+   checkName()
+   // checkSurname()
+   // checkEmail()
+   // checkAge()
+}
+
+const events = function (e) {
+   e.preventDefault()
+   validator()
+}
+
+const main = function () {
+   var form = document.getElementById('form')
+   form.addEventListener('submit', events)
+}
+
+main()
