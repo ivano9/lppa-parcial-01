500261b21406a497371c2a9a400cb243f0d2ece2
Iván Giovanazzi <ivanog@pm.me>
2020-05-24 00:39:00 -0300

    implemented functions chckSurname, checkEmail and checkAge

--- js/app.js
+++ js/app.js
@@ -16,20 +16,56 @@ const checkName = function () {
 }
 
 const checkSurname = function () {
-   //TODO
+   var { value } = document.getElementById('surname')
+   if (value === null || value === '' || value.length < 3)
+      warnStack.surname.push('El apellido debe tener al menos 3 letras')
+
+   return;
 }
 
 const checkEmail = function () {
-   //TODO
+   var { value } = document.getElementById('email')
+
+   if (value.length < 5)
+      warnStack.email.push('El email debe tener al menos 5 caracteres')
+   else {
+      // regular expression for validate email
+      var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+      if (!re.test(value))
+         warnStack.email.push('Email no valido')
+   }
+
+   return;
 }
 
 const checkAge = function () {
-   //TODO
+   var { value } = document.getElementById('age')
+   
+   var result = (value - Math.floor(value)) !== 0
+
+   if (result) 
+      warnStack.age.push('La edad debe ser un numero entero')
+   else
+      if (value < 1 || value > 99)
+         warnStack.age.push('La edad válida debe mayor a 0 y menor a 100')
+   
+   return;
 }
 
 const errorBuilder = function () {
    var errorName = document.getElementById('errorName')
-   errorName.innerHTML = warnStack.name.join('. ')
+   errorName.innerHTML = warnStack.name
+
+   var errorSurname = document.getElementById('errorSurname')
+   errorSurname.innerHTML = warnStack.surname
+
+   var errorEmail = document.getElementById('errorEmail')
+   errorEmail.innerHTML = warnStack.email.join('. ')
+
+   var errorAge = document.getElementById('errorAge')
+   errorAge.innerHTML = warnStack.age
+
    return;
 }
 
@@ -45,9 +81,9 @@ const messageCleaner = function () {
 const validator = function () {
    messageCleaner()
    checkName()
-   // checkSurname()
-   // checkEmail()
-   // checkAge()
+   checkSurname()
+   checkEmail()
+   checkAge()
    errorBuilder()
    return;
 }
